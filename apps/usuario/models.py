b5fa28438180de0a31ee97555f0fe1e73e28d2da
FedericoMeza <federicojlmeza@outlook.com>
2022-08-01 18:56:59 -0300

    correcciones

--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class Usuario(AbstractUser):
+    imagen = models.ImageField(upload_to='usuario', default='usuario/default_user.png' )
+    email = models.EmailField(null = False, blank = False)
+    facebook = models.URLField(null = True, blank = True)
+    twitter = models.URLField(null = True, blank = True)
+    instagram = models.URLField(null = True, blank = True)
