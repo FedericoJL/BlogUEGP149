7305ccd2571e163a66e1109a07d2e9903fa8c1e7
FedericoMeza <federicojlmeza@outlook.com>
2022-08-01 18:48:56 -0300

    primer commit

---
+++ blog/produccion.py
@@ -0,0 +1,21 @@
+from .settings import *
+import django_heroku
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+django_heroku.settings(locals())
