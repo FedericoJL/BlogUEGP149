771dce44760ba8d2f49ce9079837fb706851142a
FedericoMeza <federicojlmeza@outlook.com>
2022-08-03 23:38:51 -0300

    pruebas

--- apps/noticia/views.py
+++ apps/noticia/views.py
@@ -4,12 +4,10 @@ from .models import Noticia
 # Create your views here.
 
 def home(request):
-    posts = Noticia.objects.filter(destacada = True)
-    return render(request, 'index.html', {'posts':posts})
+    return render(request, 'index.html')
 
 def noticias(request):
-    posts = Noticia.objects.filter(destacada = True)
-    return render(request, 'noticias.html', {'posts':posts})
+    return render(request, 'noticias.html')
 
 def nosotros(request):
     return render(request, 'nosotros.html')
