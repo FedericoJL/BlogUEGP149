6a57a631bc5c09869e0a007c51a30a3b56ae1c26
FedericoMeza <federicojlmeza@outlook.com>
2022-08-03 23:04:09 -0300

    Correcciones

--- apps/noticia/urls.py
+++ apps/noticia/urls.py
@@ -3,6 +3,6 @@ from .views import home, noticias, nosotros
 
 urlpatterns = [
     path('', home, name = 'index'),
-    path('noticia', noticias, name = 'noticia'),
-    path('nosotros', nosotros, name = 'nosotros'),
+    path('noticia/', noticias, name = 'noticia'),
+    path('nosotros/', nosotros, name = 'nosotros'),
 ]
